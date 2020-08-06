43e660aecdf3dc360c861d2ee30a5284b99ea5f8
chenghsienyu <chenghsienyu@aim.com>
2020-08-07 00:45:06 +0800

    add window.postMessage
    data cross iframe and main page

---
+++ docs/assets/articles/postMessage.js
@@ -0,0 +1,18 @@
+ // addEventListener support for IE8 
+ function bindEvent(element, eventName, eventHandler) {
+     if (element.addEventListener) {
+         element.addEventListener(eventName, eventHandler, false);
+     } else if (element.attachEvent) {
+         element.attachEvent('on' + eventName,
+             eventHandler);
+     }
+ } // Send a message to the parent 
+ var sendMessage = function(msg) {
+     window.parent.postMessage(msg, '*');
+ };
+ var results = document.getElementById('results');
+ bindEvent(window, 'scroll', function(e) {
+     var scroll_Y = window.scrollY;
+     console.log(scroll_Y);
+     sendMessage(scroll_Y);
+ });

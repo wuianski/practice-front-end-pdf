395e1c3e7690d17616971e80e8b25dfac22a27e8
seanmars <mars0222@gmail.com>
2020-09-05 21:40:10 +0800

    Fixed the 'blob.arrayBuffer is not a function' on safari

---
+++ docs/patch-arrayBuffer.js
@@ -0,0 +1,14 @@
+(function () {
+    File.prototype.arrayBuffer = File.prototype.arrayBuffer || myArrayBuffer;
+    Blob.prototype.arrayBuffer = Blob.prototype.arrayBuffer || myArrayBuffer;
+
+    function myArrayBuffer() {
+        return new Promise((resolve) => {
+            let fr = new FileReader();
+            fr.onload = () => {
+                resolve(fr.result);
+            };
+            fr.readAsArrayBuffer(this);
+        });
+    }
+})();
